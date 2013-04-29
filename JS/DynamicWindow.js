627b4962191cc15f0f5d2c306fd0e8ca6a6a40dc
Robby <geomasta21@aol.com>
2013-04-28 22:20:49 -0500

    betterd JS simulation, fixed trajetory error

--- JS/DynamicWindow.js
+++ JS/DynamicWindow.js
@@ -1,10 +1,10 @@
 (function () {
     var CLOSE_ENOUGH_TO_GOAL = .1,
-        ANGULAR_ACCEL = 1, ANGULAR_SAMPLES = 3, ANGULAR_MIN = -1, ANGULAR_MAX = 1,
-        LINEAR_ACCEL = 1, LINEAR_SAMPLES = 3, LINEAR_MIN = 0, LINEAR_MAX = 1,
-        DT = 1, 
-        CLEARANCE_MIN = .3, CLEARANCE_MAX = 2,
-        LINEAR_WEIGHT = .1, CLEARANCE_WEIGHT = 0, GOAL_DIR_WEIGHT = 1,
+        ANGULAR_ACCEL = 2, ANGULAR_SAMPLES = 15, ANGULAR_MIN = -3, ANGULAR_MAX = 3,
+        LINEAR_ACCEL = 2, LINEAR_SAMPLES = 10, LINEAR_MIN = 0, LINEAR_MAX = 2.0,
+        DT = .8, 
+        CLEARANCE_MIN = .6, CLEARANCE_MAX = 2,
+        LINEAR_WEIGHT = .01, CLEARANCE_WEIGHT = 1, GOAL_DIR_WEIGHT = 1,
         
         euclidDist = function (x1, y1, x2, y2) {
             return Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
@@ -53,6 +53,31 @@
             
             return min_clearance;
         },
+
+        calcKinematic = function (cur_x, cur_y, cur_dir, linear, angular) {
+            var dir = cur_dir + DT*angular;
+            
+            if (Math.abs(angular) < 1e-2) {
+                x = cur_x + DT*linear*Math.cos(cur_dir);
+                y = cur_y + DT*linear*Math.sin(cur_dir);
+            } else if (angular < 0) {
+                var len = DT*linear;
+                var beta = cur_dir - dir; 
+                var R = len/beta;
+
+                x = cur_x + R*Math.cos(cur_dir - Math.PI/2) + R*Math.cos(cur_dir + Math.PI/2 - beta);
+                y = cur_y + R*Math.sin(cur_dir - Math.PI/2) + R*Math.sin(cur_dir + Math.PI/2 - beta);
+            } else if (angular > 0) {
+                var len = DT*linear;
+                var beta = dir - cur_dir; 
+                var R = len/beta;
+
+                x = cur_x + R*Math.cos(cur_dir + Math.PI/2) + R*Math.cos(cur_dir - Math.PI/2 + beta);
+                y = cur_y + R*Math.sin(cur_dir + Math.PI/2) + R*Math.sin(cur_dir - Math.PI/2 + beta);
+            } 
+            
+            return [x, y, dir];
+        },
         
         // returns true on success
         DynamicWindow = function (spec) {
@@ -77,8 +102,8 @@
             
             var options = [];
             
-            for (var ang_add = cur_angular - ANGULAR_ACCEL; 
-                 ang_add <= cur_angular + ANGULAR_ACCEL;
+            for (var ang_add = -ANGULAR_ACCEL; 
+                 ang_add <= ANGULAR_ACCEL;
                  ang_add += ANGULAR_ACCEL*2/(ANGULAR_SAMPLES - 1)) 
             {
                 var angular = cur_angular + ang_add;
@@ -87,32 +112,34 @@
                     continue;
                 }
                 
-                for (var lin_add = cur_linear - LINEAR_ACCEL; 
-                     lin_add <= cur_linear + LINEAR_ACCEL;
+                for (var lin_add = -LINEAR_ACCEL; 
+                     lin_add <= LINEAR_ACCEL;
                      lin_add += LINEAR_ACCEL*2/(LINEAR_SAMPLES - 1)) 
                 {
                     var linear = cur_linear + lin_add;
-                
+                    
                     if (linear < LINEAR_MIN || linear > LINEAR_MAX) {
                         continue;
                     } 
                     
                     // calcular new point
-                    var dir = cur_dir + DT*angular,
-                        x = cur_x + DT*linear*Math.cos(dir),
-                        y = cur_y + DT*linear*Math.sin(dir);
-                        
+                    var tmp = calcKinematic(cur_x, cur_y, cur_dir, linear, angular)
+                    x = tmp[0]
+                    y = tmp[1]
+                    dir = tmp[2]                    
+ 
                     // console.log("debug", x, y, dir);
                 
-                    Plotter.drawArrow(x, y, dir);
                            
                     // calculate clearance
                     var clearance = calcClearance(cloud, x, y);
                     
                     // ignore if the point is too close to obstacles
+                    console.log(clearance);
                     if (clearance < CLEARANCE_MIN) {
                         continue;
                     }
+                    Plotter.drawArrow(x, y, dir);
                     
                     // calculate normalized clearance weight
                     var clearanceNorm = (clearance - CLEARANCE_MIN)/CLEARANCE_MAX;
@@ -236,10 +263,15 @@
     Plotter.clear();
     Plotter.drawAxises(1, 1);
     
-    var goal_x = null, 
+    var cur_x = 0,
+        cur_y = 0,
+        cur_dir = Math.PI/2,
+        goal_x = null, 
         goal_y = null, 
         cloud_x = null,
         cloud_y = null,
+        cur_lin = 0,
+        cur_ang = 0,
         state = 0;
 
     canvas.onmousedown = function (event) {
@@ -257,28 +289,36 @@
             
             var spec = {
                 action_out: {linear: 0, angular: 0},
-                cur_x: 0,
-                cur_y: 0,
-                cur_dir: Math.PI/2,
-                cur_linear: 0,
-                cur_angular: 0,
-                cloud: [],
+                cur_x: cur_x,
+                cur_y: cur_y,
+                cur_dir: cur_dir,
+                cur_linear: cur_lin,
+                cur_angular: cur_ang,
+                cloud: [{x:1, y:1}],
                 goal_x: goal_x,
                 goal_y: goal_y,
             }
             
             Plotter.clear();
             Plotter.drawAxises(1, 1);
+            Plotter.plotPoint(1, 1);
             DynamicWindow(spec);  
             Plotter.plotPoint(x, y);
             
             var linear = spec.action_out.linear,
-                angular = spec.action_out.angular,
-                dir = Math.PI/2 + DT*angular,
-                x = DT*linear*Math.cos(dir),
-                y = DT*linear*Math.sin(dir);
-                
+                angular = spec.action_out.angular;
+
+            var tmp = calcKinematic(cur_x, cur_y, cur_dir, linear, angular);
+            x = tmp[0];
+            y = tmp[1];
+            dir = tmp[2];            
+
             Plotter.drawRedArrow(x, y, dir);
+            cur_x = x;
+            cur_y = y;
+            cur_dir = dir;
+            cur_lin = linear;
+            cur_ang = angular;
         }
     };
 })();
